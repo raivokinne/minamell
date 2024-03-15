0a3e207ae05b9d2e0d1f4546faf7d9af3fd492ab
Kevins Markuss Kanalis <120098910+KevinTheSome@users.noreply.github.com>
2024-03-15 11:08:50 +0200

    Update Database.php
    fix: added PDO attribute feach mode

--- database/Database.php
+++ database/Database.php
@@ -9,6 +9,7 @@ class Database
         $dsn = $config['driver'] . ':host=' . $config['host'] . ';port=' . $config['port'] . ';dbname=' . $config['database'];
         $this->connection = new PDO($dsn, $config['username'], $config['password']);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
 
     public function getConnection()
